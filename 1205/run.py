4c7344d96d423b9558cff482e796db8325966402
Maplesugar43251 <leann.chang.961225@gmail.com>
2024-12-19 12:04:52 +0800

    Add files via upload

--- 1205/run.py
+++ 1205/run.py
@@ -8,6 +8,11 @@ BLACK = (0,0,0)
 #載入圖片
 img_dino = pygame.image.load("dino.png")
 img_dinorun = [pygame.image.load("DinoRun1.png"),pygame.image.load("DinoRun2.png")]
+img_dinoduck = [pygame.image.load("DinoDuck1.png"),pygame.image.load("DinoDuck2.png")]
+
+img_bird = pygame.image.load("Bird1.png")
+img_birdrun = [pygame.image.load("Bird1.png"),pygame.image.load("Bird2.png")]
+
 
 img_cactus = pygame.image.load("cactus.png")
 img_dino = pygame.transform.scale(img_dino,(100,100))
@@ -18,14 +23,21 @@ dino_rect = img_dino.get_rect()
 dino_rect.x = 50
 dino_rect.y = 300
 is_jumping = False
+is_ducking = False
 jump = 20
 nowjump = jump
 g = 1
 
+bird_rect = img_bird.get_rect()
+bird_rect.x = 2000
+bird_rect.y = 250
+
 cactus_rect = img_cactus.get_rect()
-cactus_rect.x = 2000
+cactus_rect.x = 1000
 cactus_rect.y = 330
 speed = 10
+
+
 
 #設定分數
 score = 0
@@ -35,6 +47,8 @@ font = pygame.font.Font(None,36)
 clock = pygame.time.Clock()
 running = True
 gameover = False
+lastime = 0
+frame = 0
 
 
 
@@ -55,11 +69,20 @@ while running:
                 score = 0
                 cactus_rect.x=2000
                 gameover = False
+            if event.key == pygame.K_s:
+                dino_rect.y = 330
+                is_ducking = True
+        if event.type == pygame.KEYUP:
+            if is_ducking:
+                dino_rect.y = 300
+                is_ducking = False        
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             is_jumping = True
             if gameover:
                 score = 0
-                cactus_rect.x = 2000
+                cactus_rect.x = 3000
+                bird_rect.x = 2000
                 gameover = False
             
     if not gameover:
@@ -73,15 +96,22 @@ while running:
                 is_jumping = False
 
         cactus_rect.x -= speed  
+        bird_rect.x -= speed
         if cactus_rect.x < 0:
             cactus_rect.x = 1280
+        if bird_rect.x < 0:
+            bird_rect.x = 1280
             
 
         if dino_rect.colliderect(cactus_rect):
             if score > highscore:
                 highscore = score
-                
-            gameover = True    
+            gameover = True 
+
+        if dino_rect.colliderect(bird_rect):
+            if score > highscore:
+                highscore = score
+            gameover = True       
                 
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("white")
@@ -93,14 +123,30 @@ while running:
         highscore_show = font.render(f"Hi Score: {highscore}",True, BLACK)
         screen.blit(highscore_show,(10,30))
         if gameover:
-            gameover_show = font.render(f"Game Over",True, BLACK)
+            gameover_show = font.render(f"GAME OVER",True, BLACK)
             screen.blit(gameover_show,(550,150))
 
-        
+
+        #更新跑步動畫
+        nowtime = pygame.time.get_ticks()
+        if nowtime - lastime > 300: #0.3秒
+            frame = (frame+1) % 2
+            lastime = nowtime
+
+
+        if is_ducking:
+            screen.blit(img_dinoduck[frame],dino_rect)
+        else:
+            screen.blit(img_dinorun[frame],dino_rect)   
+
+
+        # screen.blit(img_dinorun[frame],dino_rect)
         # RENDER YOUR GAME HERE
-        screen.blit(img_dino,dino_rect)
+        #screen.blit(img_dino,dino_rect)
         screen.blit(img_cactus,cactus_rect)
-        
+        screen.blit(img_birdrun[frame],bird_rect)
+
+
         # flip() the display to put your work on screen
         pygame.display.flip()
 
